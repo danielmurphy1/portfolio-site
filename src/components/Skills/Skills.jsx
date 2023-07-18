9a5c340d5f4c2587d4db7432de60cf621b673c68
danielmurphy1 <daniel.murphy.1@gmail.com>
2023-07-18 12:27:04 -0400

    updated about me and skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,10 +1,9 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Fade from 'react-reveal/Fade';
 import { Container, Row, Col } from 'react-bootstrap';
 import Title from '../Title/Title';
 
 const Skills = () => {
-
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
 
@@ -24,38 +23,45 @@ const Skills = () => {
         <Title title="Skills" />
         <Row className="skills-wrapper">
           <Col md={4} sm={12}>
-            <Fade left={isDesktop} bottom={isMobile} bottom duration={1000} delay={600} distance="30px">
+            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={600} distance="30px">
               <div className="skills-wrapper__info">
-                <ul className="skills-wrapper__info-text list list-group">Front-End
-                    <li className="list-item">HTML/CSS</li>
-                    <li className="list-item">JavaScript</li>
-                    <li className="list-item">React</li>
-                    <li className="list-item">Bootstrap</li>
+                <ul className="skills-wrapper__info-text list list-group">
+                  Front-End
+                  <li className="list-item">HTML/CSS</li>
+                  <li className="list-item">JavaScript</li>
+                  <li className="list-item">React</li>
+                  <li className="list-item">Bootstrap</li>
+                  <li className="list-item">Angular</li>
                 </ul>
               </div>
             </Fade>
           </Col>
           <Col md={4} sm={12}>
             <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
-            <div className="skills-wrapper__info">
-                <ul className="skills-wrapper__info-text list list-group">Back-End
-                    <li className="list-item">Node</li>
-                    <li className="list-item">Express</li>
-                    <li className="list-item">RESTful APIs</li>
-                    <li className="list-item">SQL/PostgreSQL</li>
+              <div className="skills-wrapper__info">
+                <ul className="skills-wrapper__info-text list list-group">
+                  Back-End
+                  <li className="list-item">Node</li>
+                  <li className="list-item">Express</li>
+                  <li className="list-item">C#</li>
+                  <li className="list-item">RESTful APIs</li>
+                  <li className="list-item">SQL/PostgreSQL</li>
+                  <li className="list-item">MongoDB/NoSQL DBs</li>
                 </ul>
               </div>
             </Fade>
           </Col>
           <Col md={4} sm={12}>
             <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
-            <div className="skills-wrapper__info">
-                <ul className="skills-wrapper__info-text list list-group">Technologies/Tools
-                    <li className="list-item">Postman</li>
-                    <li className="list-item">Git/GitHub</li>
-                    <li className="list-item">Heroku</li>
-                    <li className="list-item">Netlify</li>
-                    <li className="list-item">JSON Web Token</li>
+              <div className="skills-wrapper__info">
+                <ul className="skills-wrapper__info-text list list-group">
+                  Technologies/Tools
+                  <li className="list-item">Postman</li>
+                  <li className="list-item">Git/GitHub</li>
+                  <li className="list-item">Electron</li>
+                  <li className="list-item">Netlify</li>
+                  <li className="list-item">JSON Web Token</li>
+                  <li className="list-item">Jasmine/Jest Testing Suites</li>
                 </ul>
               </div>
             </Fade>
--- src/mock/data.js
+++ src/mock/data.js
@@ -20,12 +20,14 @@ export const aboutData = {
   img: 'me.jpg',
   paragraphOne:
     'I am a software developer with a passion for problem solving. As long as I can remember, I have asked the question "Why?", and I feel that asking that question makes me a good developer and problem solver. It is not enough that I solve the problem (development or otherwise); rather, I aim to understand why the solution works and to learn from that experience with new information to further my own understanding and to help others.',
+  // paragraphTwo:
+  //   "My first venture into development/coding came during my senior year of undergrad when I took a Visual Basic course to fulfill a general education requirement. The basics I learned in that class helped me during my journey as a self-taught developer - learning more advanced languages and technologies such as HTML/CSS, JavaScript, Node, and React. As a dedicated life-long learner, each time that I sit down to work on a project or to study, I am excited knowing that I will learn something that I didn't know before.",
   paragraphTwo:
-    "My first venture into development/coding came during my senior year of undergrad when I took a Visual Basic course to fulfill a general education requirement. The basics I learned in that class helped me during my journey as a self-taught developer - learning more advanced languages and technologies such as HTML/CSS, JavaScript, Node, and React. As a dedicated life-long learner, each time that I sit down to work on a project or to study, I am excited knowing that I will learn something that I didn't know before.",
+    'I became a software developer due to my desire to continually challenge myself and my dedication to being a life-long learner. I am not certain which I love more about being a developer: the logical thinking, problem-solving thought process that comes with development or the ability to create solutions to real world problems that can both help people or bring them joy. Regardless of which it is, I do love this career, and I am happy and proud to be a self-taught developer. I work hard, I am dedicated, and I certainly follow-through.',
   paragraphThree:
     'In addition to being a developer, I am also a veteran of the United States Navy. While serving in the Navy, I was an Avionics Technician. As an AT, I had the pleasure of maintaining (AKA repairing and troubleshooting) some of the most advanced avionics suites in the world. I was routinely called upon to troubleshoot and repair some of the most difficult discrepancies. In doing so, I stretched my critical thinking and problem-solving skills. I believe that experience has helped me to think as a developer when solving problems and squishing bugs in my code. Moreover, my experience in the Navy provided me with solid teamwork and leadership skills that make me a great addition to any team environment.',
   paragraphFour: 'Lastly, as you can see, I have a pretty sweet beard, of which I am quite proud.',
-  resume: 'https://drive.google.com/file/d/10BHbAgPeKO1UQIuL-kaDz157ift-DEDZ/view?usp=sharing', // if no resume, the button will not show up
+  resume: 'https://drive.google.com/file/d/1OKt7FPNx1iQOjfOVNQc74cUPyV7lLOJN/view?usp=drive_link', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
