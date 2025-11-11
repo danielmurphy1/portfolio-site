942c9d0314911a6ee0515ef32bb7d85ce0fd58ae
bguise987 <bguise135@gmail.com>
2025-11-11 12:23:56 -0500

    Rewording: Reduce use of phrase problem solving

--- src/mock/data.js
+++ src/mock/data.js
@@ -19,7 +19,7 @@ export const heroData = {
 export const aboutData = {
   img: 'me.jpg',
   paragraphOne:
-    'I am a software developer with a passion for problem solving. As long as I can remember, I have asked the question "Why?", and I feel that asking that question makes me a good developer and problem solver. It is not enough that I solve the problem (development or otherwise); rather, I aim to understand why the solution works and to learn from that experience with new information to further my own understanding and to help others.',
+    'I am a software developer with a passion for problem solving. As long as I can remember, I have asked the question "Why?", and I feel that asking that question makes me a better developer. It is not enough that I solve the problem (development or otherwise); rather, I aim to understand why the solution works and to learn from that experience with new information to further my own understanding and to help others.',
   // paragraphTwo:
   //   "My first venture into development/coding came during my senior year of undergrad when I took a Visual Basic course to fulfill a general education requirement. The basics I learned in that class helped me during my journey as a self-taught developer - learning more advanced languages and technologies such as HTML/CSS, JavaScript, Node, and React. As a dedicated life-long learner, each time that I sit down to work on a project or to study, I am excited knowing that I will learn something that I didn't know before.",
   paragraphTwo:
